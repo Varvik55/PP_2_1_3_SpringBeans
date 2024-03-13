0137c3e9b1bb195326e976158340a4008201ec16
gribovkirill <qwertyastilark@mail.ru>
2024-03-13 23:13:44 +0400

    Commit1

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,15 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
-
+    @Qualifier("animalsCage")
     public Long getTime() {
         return nanoTime;
     }
+
 }
